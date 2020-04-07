5286cac15a29c1fbad66833426daa2b531b20237
manstefania314 <stefania.man@cerner.com>
2020-04-07 22:23:39 +0300

    Added models for Customer and Order. Added empty classes for FoodItem and DrinkItem so I can referance them in the Order class.

---
+++ RestaurantManagementSystem/RestaurantManagementSystem/models/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.models
+{
+    class Customer
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Budget { get; set; }
+
+        public Customer(int id, string firstName, string lastName, double budget)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Budget = budget;
+        }
+
+        public override string ToString()
+        {
+            return $"First name: {FirstName}    |   Last name: {LastName}   |   Budget: {Budget}";
+        }
+    }
+}
---
+++ RestaurantManagementSystem/RestaurantManagementSystem/models/Order.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantManagementSystem.helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagementSystem.models
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public Customer Customer { get; set; }
+        public Dictionary<FoodItem, int> FoodItems { get; set; }
+        public Dictionary<DrinkItem, int> DrinkItems { get; set; }
+        public double TotalSum { get; set; }
+
+        public Order(int id, Customer customer)
+        {
+            Id = id;
+            Customer = customer;
+            FoodItems = new Dictionary<FoodItem, int>();
+            DrinkItems = new Dictionary<DrinkItem, int>();
+            TotalSum = Constants.DefaultTotalSumValue;
+        }
+
+        public override string ToString()
+        {
+            return $"Customer: {Customer.ToString()}    |   Total: {TotalSum}";
+        }
+    }
+}
